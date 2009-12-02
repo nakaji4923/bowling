010299844c0f479b120579d109af944c86b1b94c
nakajima hideki <nakaji4923@gmail.com>
2009-12-02 15:17:16 +0900

    refactor Bowling#game

--- lib/bowling/game.rb
+++ lib/bowling/game.rb
@@ -16,10 +16,12 @@ module Bowling
 			roll_idx = 0
 			10.times do
 				if @rolls[roll_idx] == 10 # case strike
-					score += @rolls[roll_idx] + @rolls[roll_idx + 1] + @rolls[roll_idx + 2]
+					score += @rolls[roll_idx] # pins
+					score += @rolls[roll_idx + 1] + @rolls[roll_idx + 2] # bonus
 					roll_idx += 1
-				elsif (@rolls[roll_idx] + @rolls[roll_idx + 1]) == 10  # case strike
-					score += @rolls[roll_idx] + @rolls[roll_idx + 1] + @rolls[roll_idx + 2]
+				elsif (@rolls[roll_idx] + @rolls[roll_idx + 1]) == 10  # case spaire
+					score += @rolls[roll_idx] + @rolls[roll_idx + 1]  # pins
+					score += @rolls[roll_idx + 2] # bonus
 					roll_idx += 2
 				else # case others
 					score += @rolls[roll_idx] + @rolls[roll_idx + 1]
