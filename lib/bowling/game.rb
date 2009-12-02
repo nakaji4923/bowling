5d4f4346ca510e8c3d9911ab7870d774b68cec90
nakajima hideki <nakaji4923@gmail.com>
2009-12-02 14:23:06 +0900

    modify strike method!

--- lib/bowling/game.rb
+++ lib/bowling/game.rb
@@ -1,15 +1,29 @@
 module Bowling
 	class Game
 		def initialize
-			@score = 0
+#			@score = 0
+			@rolls = []
 		end
 
 		def roll(pins)
-			@score += pins
+#			@score += pins
+			@rolls << pins
 		end
 
 		def score
-			return @score
+#			return @score
+			score = 0
+			roll_idx = 0
+			10.times do
+				if @rolls[roll_idx] == 10 # case strike
+					score += @rolls[roll_idx] + @rolls[roll_idx + 1] + @rolls[roll_idx + 2]
+					roll_idx += 1
+				else # case others
+					score += @rolls[roll_idx] + @rolls[roll_idx + 1]
+					roll_idx += 2
+				end
+			end
+			return score
 		end
 	end
 end
